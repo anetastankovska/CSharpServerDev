5839106e21fc888fe20d34e1548a7b3d681e5e5b
Aneta Stankovska <anetastankovskaane@gmail.com>
2022-09-21 20:56:28 +0200

    Completed class 02

--- Class01/ServerDevelopment/FirstDemo/Program.cs
+++ Class01/ServerDevelopment/FirstDemo/Program.cs
@@ -1,33 +1,32 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 var address = IPAddress.Any;
-var port = 668; // the neighbour of the beast
+var port = 668; //the neighbour of the beast
+
 TcpListener listener = new TcpListener(address, port);
 listener.Start();
 
-//listener.Stop();
 Console.WriteLine("Starting server loop");
 while (true)
 {
-    //wait for a request
+    // wait for a request
     Console.WriteLine($"Waiting for tcp client");
     var client = listener.AcceptTcpClient();
     Console.WriteLine($"Accepted tcp client");
-    //process request and send response
+    // process request and get response
     using var stream = client.GetStream();
-    byte[] buffer = new byte[1024];
+    byte[] buffer = new byte[8192];
     Span<byte> bytes = new Span<byte>(buffer);
     var byteCount = stream.Read(bytes);
-
-    //send out response
-    if (byteCount == -1)
-    {
-        Console.WriteLine("The stream has ended");
-    }
-    else
-    {
-        Console.WriteLine($"We read the value {byteCount}");
-    }
-
+    // send out response
+    Console.WriteLine($"Read out {byteCount} bytes");
+    var data = Encoding.UTF8.GetString(bytes);
+    Console.WriteLine(data);
+    var responseString = @$"HTTP/1.1 200 OK
+Hello from SEDC Server";
+    var responseBytes = Encoding.UTF8.GetBytes(responseString);
+    stream.Write(responseBytes);
+    client.Close();
 }
