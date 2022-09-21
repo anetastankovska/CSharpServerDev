166fe2d4d2bc9292c8759d44829bdc2a2a87276d
Aneta Stankovska <anetastankovskaane@gmail.com>
2022-09-21 20:51:26 +0200

    Added code from Class

--- Class01/ServerDevelopment/FirstDemo/Program.cs
+++ Class01/ServerDevelopment/FirstDemo/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+var address = IPAddress.Any;
+var port = 668; // the neighbour of the beast
+TcpListener listener = new TcpListener(address, port);
+listener.Start();
+
+//listener.Stop();
+Console.WriteLine("Starting server loop");
+while (true)
+{
+    //wait for a request
+    Console.WriteLine($"Waiting for tcp client");
+    var client = listener.AcceptTcpClient();
+    Console.WriteLine($"Accepted tcp client");
+    //process request and send response
+    using var stream = client.GetStream();
+    byte[] buffer = new byte[1024];
+    Span<byte> bytes = new Span<byte>(buffer);
+    var byteCount = stream.Read(bytes);
+
+    //send out response
+    if (byteCount == -1)
+    {
+        Console.WriteLine("The stream has ended");
+    }
+    else
+    {
+        Console.WriteLine($"We read the value {byteCount}");
+    }
+
+}
