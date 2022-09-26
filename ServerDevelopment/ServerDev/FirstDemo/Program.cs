f781b7a168a19bd72e125802f36b9f17cfba499c
Aneta Stankovska <anetastankovskaane@gmail.com>
2022-09-26 23:06:23 +0200

    Added code

---
+++ ServerDevelopment/ServerDev/FirstDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var address = IPAddress.Any;
+var port = 668; //the neighbour of the beast
+
+TcpListener listener = new TcpListener(address, port);
+listener.Start();
+
+Console.WriteLine("Starting server loop");
+while (true)
+{
+    // wait for a request
+    Console.WriteLine($"Waiting for tcp client");
+    var client = listener.AcceptTcpClient();
+    Console.WriteLine($"Accepted tcp client");
+
+    // process request
+    using var stream = client.GetStream();
+    Span<byte> bytes = new Span<byte>(new byte[8192]);
+    var byteCount = stream.Read(bytes);
+    Console.WriteLine($"Read out {byteCount} bytes");
+    var data = Encoding.UTF8.GetString(bytes);
+    Console.WriteLine(data);
+
+    // get response
+    var responseString = @$"HTTP/1.1 200 OK
+Hello from SEDC Server";
+
+    // send response
+    var responseBytes = Encoding.UTF8.GetBytes(responseString);
+    stream.Write(responseBytes);
+    client.Close();
+}
---
+++ ServerDevelopment/ServerDev/Sedc.Server/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sedc.Server
+{
+    public class Server
+    {
+        public int Port { get; private set; }
+
+        public Server(ServerOptions options)
+        {
+            Port = options.Port;
+        }
+        public void Configure(object? configuration = null)
+        {
+            Console.WriteLine("Server is being configured");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Running server");
+
+            var address = IPAddress.Any;
+
+            TcpListener listener = new TcpListener(address, Port);
+            listener.Start();
+
+            while (true)
+            {
+                // wait for a request
+                var client = listener.AcceptTcpClient();
+
+                // process request
+                TcpSendReceive.ProcessRequest(client);
+
+                // send response
+                TcpSendReceive.SendResponse(client);
+            }
+        }
+    }
+}
