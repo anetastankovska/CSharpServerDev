4aa40b12176ed3ae70f011831cbc4b34f314f287
Aneta Stankovska <anetastankovskaane@gmail.com>
2022-10-05 17:04:33 +0200

    Class code

---
+++ ServerDevelopment/ServerDev/ActualServer/CustomRequestParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActualServer
+{
+    internal class CustomRequest : IRequest
+    {
+        public override string ToString()
+        {
+            return "I'm so custom, OMG!";
+        }
+    }
+    internal class CustomRequestParser : IRequestParser
+    {
+        public CustomRequestParser()
+        {
+            Console.WriteLine("CustomRequestParser reporting for duty");
+        }
+
+        public IRequest TryParse(string requestData)
+        {
+            return new CustomRequest();
+        }
+    }
+}
--- ServerDevelopment/ServerDev/Sedc.Server/Requests/IRequestParser.cs
+++ ServerDevelopment/ServerDev/Sedc.Server/Requests/IRequestParser.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sedc.Server
+namespace Sedc.Server.Requests
 {
-    public record ServerOptions(int Port, bool DevMode);
+    internal interface IRequestParser
+    {
+    }
 }
